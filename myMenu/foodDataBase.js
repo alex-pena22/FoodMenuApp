9c5612c0451a5f08a750c9ddee7a86f94eb73dd1
Alex Moreno <amoreno_tech@outlook.com>
2024-11-11 17:08:55 -0600

    changes to HTML/foodDataBaseJS/mainJS

--- myMenu/foodDataBase.js
+++ myMenu/foodDataBase.js
@@ -2,73 +2,73 @@ const foodItems = [
     {
         id:"qwerqt",
         name:"Burger",
-        price: 8,
-        image:"Mymenu/imgs/food/burger.jpeg"
+        price: 8.99,
+        image:"imgs/food/burger.jpeg"
     },
     {
         id:"zcxvas",
         name:"Dumplings",
-        price: 5,
-        image:"Mymenu/imgs/food/dumplings.jpeg"
+        price: 6.99,
+        image:"imgs/food/dumplings.jpeg"
     },
     {
         id:"ertyer",
         name:"Eggs and Bacon",
-        price: 7.99,
-        image:"Mymenu/imgs/food/eggsAndBacon.jpeg"
+        price: 8.99,
+        image:"imgs/food/eggsAndBacon.jpeg"
     },
     {
         id:"dfghdf",
         name:"Fish and Chips",
-        price: 6.99,
-        image:"Mymenu/imgs/food/fishAndChips.jpeg"
+        price: 10.99,
+        image:"imgs/food/fishAndChips.jpeg"
     },
     {
         id:"cvbnvc",
         name:"Lasagna",
-        price: 3.99,
-        image:"Mymenu/imgs/food/lasagna.jpeg"
+        price: 7.99,
+        image:"imgs/food/lasagna.jpeg"
     },
     {
         id:"tyuity",
         name:"Noddles",
         price: 4.99,
-        image:"Mymenu/imgs/food/noodles.jpeg"
+        image:"imgs/food/noodles.jpeg"
     },
     {
         id:"bnmbnm",
         name:"Pizza",
-        price: 5.99,
-        image:"Mymenu/imgs/food/pizza.jpeg"
+        price: 4.99,
+        image:"imgs/food/pizza.jpeg"
     },
     {
         id:"iopuio",
         name:"Salad",
         price: 6.99,
-        image:"Mymenu/imgs/food/Salad.jpeg"
+        image:"imgs/food/Salad.jpeg"
     },
     {
         id:"zxcvzxcv",
         name:"Spaghetti",
-        price: 10.99,
-        image:"Mymenu/imgs/food/spaghetti.jpeg",
+        price: 9.99,
+        image:"imgs/food/spaghetti.jpeg",
     },
     {
         id:"qwerqwer",
         name:"Steak",
         price: 19.99,
-        image:"Mymenu/imgs/food/steak.jpeg",
+        image:"imgs/food/steak.jpeg",
     },
     {
         id:"lkjhlk",
         name:"Sushi",
-        price: 9.99,
-        image:"Mymenu/imgs/food/sushi.jpeg"
+        price: 10.99,
+        image:"imgs/food/sushi.jpeg"
     },
     {
         id:"mnbvnm",
         name:"Tacos",
         price: 9.99,
-        image:"Mymenu/imgs/food/tacos.jpeg",
+        image:"imgs/food/tacos.jpeg",
     }
 ]
--- myMenu/myMenu.js
+++ myMenu/myMenu.js
@@ -1,80 +1,9 @@
 // Purpose is  To create a menu for the user to select and add items to a cart 
+// and then display the items in the cart and allow the user to remove items from the cart
 
-const foodItems = [
-    {
-        id:"qwerqt",
-        name:"Burger",
-        price: 8.99,
-        image:"imgs/food/burger.jpeg"
-    },
-    {
-        id:"zcxvas",
-        name:"Dumplings",
-        price: 5.99,
-        image:"imgs/food/dumplings.jpeg"
-    },
-    {
-        id:"ertyer",
-        name:"Eggs and Bacon",
-        price: 12.99,
-        image:"imgs/food/eggsAndBacon.jpeg"
-    },
-    {
-        id:"dfghdf",
-        name:"Fish and Chips",
-        price: 8.99,
-        image:"imgs/food/fishAndChips.jpeg"
-    },
-    {
-        id:"cvbnvc",
-        name:"Lasagna",
-        price: 5.99,
-        image:"imgs/food/lasagna.jpeg"
-    },
-    {
-        id:"tyuity",
-        name:"Noddles",
-        price: 4.99,
-        image:"imgs/food/noodles.jpeg"
-    },
-    {
-        id:"bnmbnm",
-        name:"Pizza",
-        price: 11.99,
-        image:"imgs/food/pizza.jpeg"
-    },
-    {
-        id:"iopuio",
-        name:"Salad",
-        price: 6.99,
-        image:"imgs/food/Salad.jpeg"
-    },
-    {
-        id:"sphvzxcv",
-        name:"Spaghetti",
-        price: 9.99,
-        image:"imgs/food/spaghetti.jpeg",
-    },
-    {
-        id:"sterqwer",
-        name:"Steak",
-        price: 19.99,
-        image:"imgs/food/steak.jpeg",
-    },
-    {
-        id:"suhjpn",
-        name:"Sushi",
-        price: 9.99,
-        image:"imgs/food/sushi.jpeg"
-    },
-    {
-        id:"mxtcvb",
-        name:"Tacos",
-        price: 9.99,
-        image:"imgs/food/tacos.jpeg",
-    }
-]
-//// basket and order cart 
+
+
+//// BASKET[] array to store the items in the cart Using local storage
 let basket = JSON.parse(localStorage.getItem('cartData')) || [];
 
 // HTMl Elements
@@ -312,7 +241,7 @@ let checkOut = () => {
             let { item,id } = x;
             let search = foodItems.find((y) => y.id === id) || [];
             return item * search.price 
-            }).reduce((x,y)=>x+y,0).toFixed(2);
+            }).reduce((x,y)=>x+y,0).toFixed(2); ``
         clearCart();
         alert("Your order of $"+ amount +" is being proccessed. Thank you for shopping with eFood!");
     }
